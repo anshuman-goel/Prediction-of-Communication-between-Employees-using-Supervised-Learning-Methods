aaddb18ab0847f547ae10b2d123950793b72cc72
rnambis <rnambis@ncsu.edu>
2016-11-22 23:22:02 -0500

    added more comments to mail_list.py

--- mail_list.py
+++ mail_list.py
@@ -3,25 +3,29 @@ import re
 from difflib import SequenceMatcher as sm
 import numpy as np
 import collections
+#the root location of the Enron email dataset
 maildir = '/home/rohit/maildir'
-alter_names = {}
-alter = []
-#reading the alternative names into a hash
+#Opening the file that contains the alternate names. It is used as a hash map to remove the inconsistencies in the email data.
 alter_names = open("alter_names.csv",'r')
+#the dictionary containing the alternative names.
 names_mapper = {}
+#reading the alternative names into a hash
 for line in alter_names.readlines():
 	split = line.split(':')
 	names_mapper[split[0]] = split[1]
+#regex for to address	
 to = re.compile("X-To:(.*)")
+#regex for from address
 x_from = re.compile("X-From:(.*)")
+#regex for date
 date = re.compile("Date:(.*)")
+#the file containing the final dataset
 mail_list = open("email_processed"+".csv",'w')
+#iterating through the inbox directories of each employees
 for subdirs in os.listdir(maildir):
 	
-	alter = []
 	sender = ""
 	reciever = ""
-	#mail_list = open("preprocessed/"+subdirs+".csv",'w') # the mail list files for each employee
 	#getting mail history from inbox
 	inbox = "/home/rohit/maildir/"+subdirs+"/inbox/"
 	if os.path.isdir(inbox) != True:
@@ -34,11 +38,11 @@ for subdirs in os.listdir(maildir):
 				continue
 			multiple = 1	
 			email = email_file.readlines()
-			#print "files"
 			for line in email:
-				#print "line"
 				
+				#finding to address
 				if to.match(line):
+					#extracting emaail address
 					search_list = re.sub("X-To:","",line)
 					reciever = re.sub("<(.+)","",search_list)
 					reciever = reciever.strip(' \t\n\r')
@@ -56,13 +60,14 @@ for subdirs in os.listdir(maildir):
 							reciever = key
 							break
 													
-					#print "to"
 					
+				#finding from address	
 				if x_from.match(line):
-					#removing the entries with multiple email addresses
+					#extacting the email addresses
 					search_list = re.sub("X-From:","",line)
 					sender = re.sub("<(.+)","",search_list)
 					sender = sender.strip(' \t\n\r')
+					#removing the entries with multiple email addresses
 					letters = collections.Counter(sender)
 					if(letters["'"] > 2 or letters[","] > 1 or letters["@"] > 1):
 						print sender
@@ -75,14 +80,15 @@ for subdirs in os.listdir(maildir):
 						if key in sender or sender in names_mapper[key]:
 							sender = key
 							break
-							
+				#finding date field			
 				if date.match(line):
+					#extracting date
 					search_list = re.sub("Date:","",line)
 					email_date = search_list.strip('\t\n\r')		
 					email_date = re.sub(r'^"|"$', '', email_date)
 					
 					#print "from
-			
+			# writes to file only if there is no multiple email addresses in to or from
 			if multiple == 1:		
 				mail_list.write(reciever+"|"+sender+"|"+email_date+"\n")
 			
